46229593ddd1b6a807d6709b4fa5f5d638807a85
joffe123 <jonas1ferkingstad@gmail.com>
2023-08-16 12:01:54 +0200

    Add files via upload

---
+++ scripts/dyreprogram.js
@@ -0,0 +1,11 @@
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Grosser_Panda.JPG") {
+    myImage.setAttribute("src", "images/panda2.jpg"); 
+  } else{
+    myImage.setAttribute("src", "images/Grosser_Panda.JPG");
+   }
+};
